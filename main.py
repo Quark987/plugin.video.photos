e8850387fb4c3dd72c011b10be8f2d9f7f6ffee4
Bert De Deckere <bert.dedeckere@icloud.com>
2021-08-31 10:14:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+# Module: default
+# Author: Bert De Deckere
+# Created on: 10.2020
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
+import sys
+import os
+from urllib.parse import urlencode, quote, parse_qsl
+import xbmc
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+from synology import SynologyPhotos
+
+# Get the plugin url in plugin:// notation.
+_url = sys.argv[0]
+# Get the plugin handle as an integer number.
+_handle = int(sys.argv[1])
+addon = xbmcaddon.Addon('plugin.video.photos')
+photos = SynologyPhotos()
+
+category_titles = {'person':'People', 'shared':'Shared', 'concept':'Subjects', 'geocoding':'Places', 'recently_added':'Recently added',
+            'general_tag':'Tags', 'album':'Personal albums', 'video':'Videos', 'search':'Search'}
+
+def get_url(**kwargs):
+    """
+    Create a URL for calling the plugin recursively from the given set of keyword arguments.
+
+    :param kwargs: "argument=value" pairs
+    :type kwargs: dict
+    :return: plugin call URL
+    :rtype: str
+    """
+    return '{0}?{1}'.format(_url, urlencode(kwargs))
+
+
+def list_categories():
+    xbmcplugin.setPluginCategory(_handle, 'My Album Collection')
+    xbmcplugin.setContent(_handle, 'images')
+    categories = photos.get_categories()
+
+    for category in categories:
+        list_item = xbmcgui.ListItem(label=category_titles[category])
+
+        addon_dir = xbmc.translatePath( addon.getAddonInfo('path') )
+        art_url = os.path.join(addon_dir, 'resources', 'icon_'+category+'.png')
+        
+        list_item.setArt({'thumb': art_url,
+                          'icon': art_url,
+                          'fanart': art_url})
+
+        url = get_url(action='show_category', category=category)
+        is_folder = True
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_albums(category, keyword=None):
+    xbmcplugin.setPluginCategory(_handle, category)
+    xbmcplugin.setContent(_handle, 'videos')
+    albums = photos.get_albums(category, keyword)
+    
+    for k in range(len(albums)):
+        list_item = xbmcgui.ListItem(label=albums[k]['name'])
+
+        list_item.setArt({'thumb': albums[k]['url'],
+                          'icon': albums[k]['url'],
+                          'fanart': albums[k]['url']})
+                          
+        list_item.setInfo('video', {'title': albums[k]['name'],
+                                    'mediatype': 'video'})
+
+        try:
+            passphrase = albums[k]['passphrase']
+        except:
+            passphrase = ''
+
+        url = get_url(action='show_album', list_id=urlencode({category+'_id':albums[k]['id']}), passphrase=passphrase)
+        is_folder = True
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_photos(list_id, keyword=None, passphrase=None):        # Keyword for searches, passphrase for shared_with_me albums
+    xbmcplugin.setPluginCategory(_handle, list_id)
+    xbmcplugin.setContent(_handle, 'images')
+    photolist = photos.get_photos(list_id, keyword, passphrase)
+
+    for k in range(len(photolist)):
+        list_item = xbmcgui.ListItem(label=photolist[k]['filename'])
+
+        list_item.setArt({'thumb': photolist[k]['url'],
+                          'icon': photolist[k]['url'],
+                          'fanart': photolist[k]['url']})
+
+        if photolist[k]['type'] == 'video':
+            list_item.setProperty('IsPlayable', 'true')
+            list_item.setInfo('video', {'title': photolist[k]['filename'],
+                                    'mediatype': 'video'})
+
+            try:
+                quality = photolist[k]['additional']['video_convert'][0]['quality']
+            except:
+                quality = 'medium'
+            
+            video_id = str(photolist[k]['additional']['thumbnail']['unit_id'])
+
+            url = photos.get_video_url(video_id, quality)
+        else:
+            list_item.setInfo('pictures', {'title':photolist[k]['filename']})
+
+            photo_cache_key = photolist[k]['additional']['thumbnail']['cache_key']
+            photo_id = str(photolist[k]['additional']['thumbnail']['unit_id'])
+
+            url = photos.get_photo_url(list_id, photo_id, photo_cache_key, passphrase)
+
+            list_item.setMimeType('image/'+photolist[k]['filename'].split('.')[-1])    # Predefine the mime type, otherwise it takes ages
+            
+        is_folder = False
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_shared():
+    xbmcplugin.setPluginCategory(_handle, 'Shared')
+    xbmcplugin.setContent(_handle, 'images')
+
+    list_item = xbmcgui.ListItem(label='Shared with others')
+    url = get_url(action='shared_with_others')
+    is_folder = True
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    list_item = xbmcgui.ListItem(label='Shared with me')
+    url = get_url(action='shared_with_me')
+    is_folder = True
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_search_results(keyword):
+    xbmcplugin.setPluginCategory(_handle, 'Search')
+    xbmcplugin.setContent(_handle, 'images')
+
+    list_item = xbmcgui.ListItem(label='Albums')
+    url = get_url(action='search_albums', keyword=keyword)
+    is_folder = True
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    list_item = xbmcgui.ListItem(label='Items')
+    url = get_url(action='search_items', keyword=keyword)
+    is_folder = True
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_search_albums(keyword):
+    return list_albums('search', keyword)
+
+
+def list_search_items(keyword):
+    return list_photos('search', keyword)
+
+
+def get_user_input():   
+    kb = xbmc.Keyboard()
+    kb.doModal()            # Onscreen keyboard appears
+    if not kb.isConfirmed():
+        return
+    query = kb.getText()    # User input
+    return query
+
+def router(paramstring):
+    """
+    Router function that calls other functions
+    depending on the provided paramstring
+
+    :param paramstring: URL encoded plugin paramstring
+    :type paramstring: str
+    """
+    # Parse a URL-encoded paramstring to the dictionary of
+    # {<parameter>: <value>} elements
+    params = dict(parse_qsl(paramstring))
+    if params and 'action' in params.keys():        # Check the parameters passed to the plugin
+        if params['action'] == 'show_category':
+            if (params['category'] == 'recently_added') or (params['category'] == 'video'):
+                list_photos(params['category'])     # There are no albums in recently added or video
+            elif params['category'] == 'search':
+                keyword = get_user_input()
+                if keyword is not None and len(keyword)>0:
+                    list_search_results(keyword)
+            elif params['category'] == 'shared':
+                list_shared()
+            else:
+                list_albums(params['category'])     # List albums within each category
+        elif params['action'] == 'search_albums':
+            list_search_albums(params['keyword'])
+        elif params['action'] == 'search_items':
+            list_search_items(params['keyword'])
+        elif params['action'] == 'show_album':
+            if 'passphrase' in params.keys():
+                list_photos(params['list_id'], passphrase=params['passphrase'])
+            else:
+                list_photos(params['list_id'])
+        elif params['action'] == 'shared_with_others':
+            list_albums('shared_with_others')
+        elif params['action'] == 'shared_with_me':
+            list_albums('shared_with_me')
+        else:
+            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+    else:
+        # If the plugin is called from Kodi UI without any parameters,
+        # display the list of video categories
+        list_categories()
+
+
+if __name__ == '__main__':
+    # Call the router function and pass the plugin call parameters to it.
+    # We use string slicing to trim the leading '?' from the plugin call paramstring
+    addon = xbmcaddon.Addon()
+
+    response = photos.login(addon.getSetting('username'),
+                            addon.getSetting('password'),
+                            addon.getSetting('host'),
+                            addon.getSetting('port'),
+                            addon.getSetting('shared library'))
+
+    if response == 'success':
+        router(sys.argv[2][1:])
+    else:
+        xbmcgui.Dialog().ok('Synology photos', response)
+
+        addon.openSettings()
+        xbmc.executebuiltin("Dialog.Close(all)")
+        #router(sys.argv[2][1:])
---
+++ synology.py
@@ -0,0 +1,191 @@
+import requests
+from urllib.parse import urlencode, quote
+
+class SynologyPhotos(object):
+
+    def __init__(self):
+        self.session = requests.session()
+
+
+    def login(self, username, password, nas_name, nas_port, browse_shared_lib):
+        self.username = username
+        self.password = password
+        self.nas_name = nas_name
+        self.nas_port = nas_port
+        self.browse_shared_lib = (browse_shared_lib == 'true')
+
+        url = f'http://{nas_name}:{nas_port}/webapi/entry.cgi'
+        data = {'api':'SYNO.API.Auth',
+                'method':'login',
+                'version':'7',
+                'enable_device_token':'no',
+                'enable_syno_token':'yes',
+                'passwd':password,
+                'account':username}
+
+        response = self.session.post(url, data=data, verify=False)
+
+        if 'success' in response.json() and response.json()['success'] == True:
+            self.cookies = response.cookies
+            self.syno_token = response.json()['data']['synotoken']
+            self.headers = {'Connection':'keep-alive', 'X-SYNO-TOKEN' : self.syno_token}
+       
+            self.api = 'SYNO.Foto'
+            return 'success'
+
+        return 'Failed to log in'
+
+
+    def kodi_header(self):
+        header = ''             # This is to transmit the cookie in Kodi format
+        for element in self.cookies.get_dict():
+            header += element + '=' + self.session.cookies[element] + ';'
+        header = header[:-1]    # Drop trailing semicolon
+
+        return '|'+'Cookie='+quote(header)+'&'+urlencode(self.session.headers)      # also add the standard headers
+
+
+    def shared_library_populated(self):
+        url = 'http://{}:{}/webapi/entry.cgi'.format(self.nas_name, self.nas_port)
+        data = {'api':'SYNO.PhotoTeam.Browse.Timeline', 'method':'get', 'version':1}
+
+        try:
+            photos = self.session.post(url, data=data, verify=False, cookies=self.cookies, headers=self.headers).json()
+            if len(photos['data']['list']) > 0:
+                return True
+        except:
+            return False
+        
+        return False
+
+
+    def get_categories(self):
+        url = 'http://{}:{}/webapi/entry.cgi'.format(self.nas_name, self.nas_port)
+        data = {'api':self.api+'.Browse.Category', 'method':'get', 'version':1}
+
+        categories = self.session.post(url, data=data, verify=False, cookies=self.cookies, headers=self.headers).json() # list the albums
+        
+        temp = ['album']
+        for element in categories['data']['list']:
+            temp.append(element['id'])
+
+        temp.append('search')
+        return temp
+
+
+    def get_albums(self, category, keyword=None):
+        url = 'http://{}:{}/webapi/entry.cgi'.format(self.nas_name, self.nas_port)
+
+        # video and recently_added are immediately forwarded to get_photos as there are no subalbums
+        params = {'api':self.api+'.Thumbnail', 'type':'"unit"', 'size':'sm', 'method':'get','version':1, 'SynoToken':self.syno_token}
+        if category == 'shared_with_others':    # Depending on the category, a different request is send
+            data = {'api':self.api+'.Browse.Album', 'method':'list', 'version':1, 'limit':500, 'shared':'true', 
+                'offset':'0', 'sort_by':'create_time', 'sort_direction':'desc', 'additional':'["thumbnail"]'}
+        elif category == 'shared_with_me':
+            data = {'api':self.api+'.Sharing', 'method':'list_shared_with_me', 'version':1, 'limit':500, 'offset':0, 
+                'additional':'["thumbnail", "sharing_info"]'}
+        elif category =='person':
+            data = {'api':self.api+'.Browse.Person', 'method':'list', 'version':1, 'limit':500, 'offset':0, 
+                'additional':'["thumbnail"]', 'show_hidden':'false'}
+            params = {'api':self.api+'.Thumbnail', 'type':'"person"', 'method':'get','version':1, 'SynoToken':self.syno_token}
+        elif category == 'concept':
+            data = {'api':self.api+'.Browse.Concept', 'method':'list', 'version':1, 'limit':500, 'offset':0, 
+                'additional':'["thumbnail"]'}
+        elif category == 'geocoding':
+            data = {'api':self.api+'.Browse.Geocoding', 'method':'list', 'version':1, 'limit':500, 'offset':0, 
+                'additional':'["thumbnail"]'}
+        elif category == 'general_tag':
+            data = {'api':self.api+'.Browse.GeneralTag', 'method':'list', 'version':1, 'limit':500, 'offset':0, 
+                'additional':'["thumbnail"]'}
+        elif category == 'search':
+            data = {'api':self.api+'.Search.Search', 'method':'list_album', 'version':1, 'limit':500, 'offset':0, 
+                'additional':'["thumbnail"]', 'keyword': keyword}
+        else:       # List personal albums, is not a synology category
+            data = {'api':self.api+'.Browse.Album', 'method':'list', 'version':1, 'limit':500, \
+                'offset':'0', 'sort_by':'create_time', 'sort_direction':'desc', 'additional':'["thumbnail"]'}
+
+        albums = self.session.post(url, data=data, verify=False, cookies=self.cookies, headers=self.headers).json() # list the albums
+        kodi_header = self.kodi_header()
+
+        for k in range(len(albums['data']['list'])):
+            if albums['data']['list'][k]['item_count'] > 0:
+                if category == 'person':
+                    params['id'] = str(albums['data']['list'][k]['id'])
+                else:
+                    params['id'] = str(albums['data']['list'][k]['additional']['thumbnail']['unit_id'])
+                params['cache_key'] = str(albums['data']['list'][k]['additional']['thumbnail']['cache_key'])
+
+                if 'passphrase' in albums['data']['list'][k].keys():                    # Required for shared_with_me albums
+                    params['passphrase'] = albums['data']['list'][k]['passphrase']
+
+                albums['data']['list'][k]['url'] = url + '?' + urlencode(params) + kodi_header
+            else:
+                albums['data']['list'][k]['url'] = ''
+        
+        return albums['data']['list']
+
+
+    def get_photos(self, list_id, keyword=None, passphrase=None):
+        url = 'http://{}:{}/webapi/entry.cgi'.format(self.nas_name, self.nas_port)
+
+        if list_id == 'recently_added':
+            data = {'api':self.api+'.Browse.RecentlyAdded', 'method':'list', 'version':1, 'limit':5000, 'offset':0, 
+                    'additional':'["thumbnail","resolution","orientation","video_convert","video_meta"]'}
+        elif list_id == 'video':
+            data = {'api':self.api+'.Browse.Item', 'method':'list', 'version':1, 'limit':5000, 'offset':0, 'type':'video', 
+                'additional':'["thumbnail","resolution","orientation","video_convert","video_meta"]'}
+        elif list_id == 'search':
+            data = {'api':self.api+'.Search.Search', 'method':'list_item', 'version':1, 'limit':5000, 'offset':0, 'keyword':keyword, 
+                'additional':'["thumbnail","resolution","orientation","video_convert","video_meta"]'}
+        elif passphrase is not None:    # shared_with_me photos
+            data = {'api':self.api+'.Browse.Item', 'method':'list', 'version':1, 'limit':5000, 'passphrase':passphrase,
+                'offset':0, 'additional':'["thumbnail","resolution","orientation","video_convert","video_meta"]'}
+        else:
+            data = {'api':self.api+'.Browse.Item', 'method':'list', 'version':1, 'limit':5000, 
+                    'offset':0, 'additional':'["thumbnail","resolution","orientation","video_convert","video_meta"]'}
+
+            id_key, id_value = list_id.split('=')
+            if id_key.startswith('search') or id_key.startswith('shared'):
+                id_key = 'album_id'
+
+            data[id_key] = id_value # Add the proper id (geocoding_id, person_id, album_id, ...)
+
+        photos = self.session.post(url, data=data, verify=False, cookies=self.cookies, headers=self.headers).json() # list the photos (and videos)
+
+        params = {'api':self.api+'.Thumbnail', 'type':'"unit"', 'size':'sm', 'method':'get','version':1, 'SynoToken':self.syno_token}
+
+        if passphrase is not None:
+            params['passphrase'] = passphrase
+
+        kodi_header = self.kodi_header()
+
+        for k in range(len(photos['data']['list'])):
+            params['id'] = str(photos['data']['list'][k]['additional']['thumbnail']['unit_id'])
+            params['cache_key'] = str(photos['data']['list'][k]['additional']['thumbnail']['cache_key'])
+            photos['data']['list'][k]['url'] = url + '?' + urlencode(params) + kodi_header
+
+        return photos['data']['list']
+    
+
+    def get_photo_url(self, list_id, photo_id, photo_cache_key, passphrase=None):
+        kodi_header = self.kodi_header()
+
+        params = {'api':self.api+'.Thumbnail', 'method':'get', 'version':1, 'SynoToken':self.syno_token, \
+                'size':'xl', 'cache_key':photo_cache_key, 'id':photo_id, 'type':'unit'}
+        
+        if passphrase is not None:
+            params['passphrase'] = passphrase
+
+        base_url = "http://{}:{}/webapi/entry.cgi.jpg?".format(self.nas_name, self.nas_port)
+
+        return base_url + urlencode(params) + kodi_header
+
+
+    def get_video_url(self, video_id, quality):
+        kodi_header = self.kodi_header()
+
+        params = {'type':'item', 'quality':'\"'+quality+'\"','use_mov':'true', 'api':self.api+'.Streaming',
+            'method':'streaming', 'version':'1', 'SynoToken':self.syno_token, 'id':video_id}
+        base_url = "http://{}:{}/webapi/entry.cgi?".format(self.nas_name, self.nas_port)
+
+        return base_url + urlencode(params) + kodi_header
